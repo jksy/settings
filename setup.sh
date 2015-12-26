525cd5489a983208ff3a0e2bb970f6bf1d1fd363
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2015-12-26 12:02:18 +0000

    ignore .git directory

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 cd $HOME/settings
 for f in $HOME/settings/.*; do
-  ln -s $HOME/settings/`basename $f` $HOME/`basename $f`
+  basename=`basename $f`
+  if [ $basename != '.git' ]; then
+    ln -s $HOME/settings/`basename $f` $HOME/`basename $f`
+  fi
 done;
