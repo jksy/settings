c20bb5d567d831d153c25a7f3874ee6dde4c5252
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2016-03-07 11:22:57 +0900

    Update install_rbenv.sh

--- install_rbenv.sh
+++ install_rbenv.sh
@@ -1,6 +1,8 @@
 sudo yum -y install git openssl-devel readline-devel zlib-devel libcurl-devel bzip2 gcc
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 export PATH=$PATH:~/.rbenv/bin
+echo eval "\$(rbenv init -)" >> ~/.bash_profile
 eval "$(rbenv init -)"
 rbenv install 2.3.0
