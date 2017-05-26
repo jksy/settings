3be7b025f91c25baf36ec2193b3e477725fd4caa
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2017-05-26 14:44:43 +0900

    Update install_rbenv.sh

--- install_rbenv.sh
+++ install_rbenv.sh
@@ -1,10 +1,10 @@
 sudo yum -y install git openssl-devel readline-devel zlib-devel libcurl-devel bzip2 gcc
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 export PATH=$PATH:~/.rbenv/bin
 eval "$(rbenv init -)"
-#echo eval "\$(rbenv init -)" >> ~/.bash_profile
+echo eval "\"\$(rbenv init -)\"" >> ~/.bash_profile
 rbenv install 2.3.0
 mkdir -p bash_completion.d
 wget -O bash_completion.d/rake https://raw.githubusercontent.com/ai/rake-completion/master/rake
